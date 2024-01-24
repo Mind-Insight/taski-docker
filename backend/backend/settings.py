38e87e9368f0ddcc84b23d7a95738be22dc8be4a
Timofey <nsk.timka2077@gmail.com>
2024-01-24 15:52:30 +0200

    some changes in configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["taskiii-docker.ddns.net"]
+ALLOWED_HOSTS = ["taskiii-docker.ddns.net", "84.201.164.58", "localhost", "127.0.0.1"]
 
 
 # Application definition
